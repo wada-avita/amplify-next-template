ea7ce9e8b017f1bdfdc304bbc520984c608b03e9
wada <wada@avita.co.jp>
2024-12-02 04:14:54 +0000

    fix: fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,13 @@ import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App() {
   return (
     <Authenticator>
       {({ signOut, user }) => (
         <main>
           <h1>Hello {user?.username}</h1>
           <button onClick={signOut}>Sign out</button>
-          <Component {...pageProps} />
         </main>
       )}
     </Authenticator>
